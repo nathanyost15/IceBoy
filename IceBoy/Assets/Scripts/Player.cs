5755cf4dcb3edb1677c5f78444047c654114915a
Nathaniel Yost <nathanyost15@gmail.com>
2018-12-21 14:37:13 -0500

    Able to Jump
    Air movement while jumping implemented, flips image depending on right or left movement.

--- IceBoy/Assets/Scripts/Player.cs
+++ IceBoy/Assets/Scripts/Player.cs
@@ -6,29 +6,36 @@ public class Player : MonoBehaviour
 {
     private enum States {Idle, Walking, Jumping, Falling, Grounded};
     private States activeState;
-    private float walkSpeed, jumpForce;
+    private float walkSpeed, airSpeed, jumpForce;
+    private SpriteRenderer sprite;
     void Start()
     {
         activeState = States.Idle;
-        walkSpeed = .5f;
-        jumpForce = 2000f;
+        walkSpeed = .7f;
+        airSpeed = walkSpeed - .2f;
+        jumpForce = 10000f;
+        sprite = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
-    {          
+    {
+        // State system
         switch(activeState)
         {
             case States.Idle:
             case States.Walking:
+                // Move Right/Left on the screen
                 if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
                 {
                     transform.position += new Vector3(walkSpeed * Time.deltaTime, 0, 0);
+                    sprite.flipX = false;
                     activeState = States.Walking;
                 }
                 else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
                 {
                     transform.position += new Vector3(-1 * walkSpeed * Time.deltaTime, 0, 0);
+                    sprite.flipX = true;
                     activeState = States.Walking;
                 }
                 else
@@ -36,6 +43,7 @@ public class Player : MonoBehaviour
                     activeState = States.Idle;
                 }
 
+                // Change state to a jump state
                 if (Input.GetKey(KeyCode.Space))
                 {                    
                     activeState = States.Jumping;                    
@@ -49,6 +57,32 @@ public class Player : MonoBehaviour
                 // Then change state to falling
                 activeState = States.Falling;
                 break;
+            case States.Falling:
+                // Air movement in horizontal direction
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.position += new Vector3(airSpeed * Time.deltaTime, 0, 0);
+                    sprite.flipX = false;
+                }
+                else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.position += new Vector3(-1 * airSpeed * Time.deltaTime, 0, 0);
+                    sprite.flipX = true;
+                }
+                break;
+            case States.Grounded:
+                // Resets player to idle state to allow jumping again
+                activeState = States.Idle;
+                break;
         }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if falling to see if the player has touched the ground to allow jumping again
+        if (activeState == States.Falling && collision.transform.tag == "Ground")
+        {
+            activeState = States.Grounded;
+        }
     }
 }
