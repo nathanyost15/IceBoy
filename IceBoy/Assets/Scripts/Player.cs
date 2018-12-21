1b1a84e89fb1cbf0b675c18097418a8cb614423b
Nathaniel Yost <nathanyost15@gmail.com>
2018-12-21 01:07:54 -0500

    Horiztonal Movement, Created States sys.
    State system that tracks what state the player is in at any given time, and horizontal movement is working. Jumping is still WIP.

--- IceBoy/Assets/Scripts/Player.cs
+++ IceBoy/Assets/Scripts/Player.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private enum States {idle};
-    private States activeState;    
-    private float idleAnimSpeed;
-    private int idleHash;
-    private Animator anim;
+    private enum States {Idle, Walking, Jumping, Falling, Grounded};
+    private States activeState;
+    private float walkSpeed, jumpForce;
     void Start()
     {
-        idleHash = Animator.StringToHash("Test1");
-        Debug.Log(idleHash);
-        //activeState = States.idle;
-        anim = GetComponent<Animator>();
-        //anim.speed = 2f;
-        anim.SetTrigger(idleHash);
+        activeState = States.Idle;
+        walkSpeed = .5f;
+        jumpForce = 2000f;
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        /*switch(activeState)
+    {          
+        switch(activeState)
         {
-            case States.idle:
-                Debug.Log("Hello");
-                anim.SetTrigger(idleHash);
+            case States.Idle:
+            case States.Walking:
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.position += new Vector3(walkSpeed * Time.deltaTime, 0, 0);
+                    activeState = States.Walking;
+                }
+                else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.position += new Vector3(-1 * walkSpeed * Time.deltaTime, 0, 0);
+                    activeState = States.Walking;
+                }
+                else
+                {
+                    activeState = States.Idle;
+                }
+
+                if (Input.GetKey(KeyCode.Space))
+                {                    
+                    activeState = States.Jumping;                    
+                }
                 break;
-        }*/
+            case States.Jumping:
+                // Applies force
+                Rigidbody2D rgb2d = gameObject.GetComponent<Rigidbody2D>();
+                rgb2d.AddForce(new Vector2(0, jumpForce * Time.deltaTime));
+
+                // Then change state to falling
+                activeState = States.Falling;
+                break;
+        }        
     }
 }
