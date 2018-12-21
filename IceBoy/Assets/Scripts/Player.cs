56847f583d9373e8736cf1d976d1e85e00d68309
Nathaniel Yost <nathanyost15@gmail.com>
2018-12-20 22:07:40 -0500

    Intialized Project Repo

---
+++ IceBoy/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private enum States {idle};
+    private States activeState;    
+    private float idleAnimSpeed;
+    private int idleHash;
+    private Animator anim;
+    void Start()
+    {
+        idleHash = Animator.StringToHash("Test1");
+        Debug.Log(idleHash);
+        //activeState = States.idle;
+        anim = GetComponent<Animator>();
+        //anim.speed = 2f;
+        anim.SetTrigger(idleHash);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        /*switch(activeState)
+        {
+            case States.idle:
+                Debug.Log("Hello");
+                anim.SetTrigger(idleHash);
+                break;
+        }*/
+    }
+}
