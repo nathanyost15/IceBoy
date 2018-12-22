ca4d888d5938268e974256161512404d9ac3edac
Nathaniel Yost <nathanyost15@gmail.com>
2018-12-22 14:28:13 -0500

    Freeze Ray Flips on x and emitts ice.
    Freeze ray will flip along the x axis as will the ice particle emitter. Still WIP as the particles emitted flip along with the emitter.

--- IceBoy/Assets/Scripts/FreezeRay.cs
+++ IceBoy/Assets/Scripts/FreezeRay.cs
@@ -6,25 +6,53 @@ public class FreezeRay : MonoBehaviour
 {
     private SpriteRenderer iceBoySprite, sprite;
     private Vector3 rightPosition, leftPosition;
+    private GameObject particleSys;
+    private ParticleSystem p;
     void Start()
     {
         leftPosition = new Vector3(-0.069f, 0.004999995f, 0);
         rightPosition = new Vector3(0.06800008f, 0.004999995f, 0);
+
+        // Get handle to Player spriterenderer
         iceBoySprite = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
-        sprite = gameObject.GetComponent<SpriteRenderer>();        
+
+        // Get handle to spriterenderer on this object
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+
+        // Get handle to particle effect system
+        particleSys = GameObject.Find("FreezeRayParticleSys");
+        p = particleSys.GetComponent<ParticleSystem>();
+        p.Stop();
     }
     
     void Update()
     {
         if(iceBoySprite.flipX)
         {
+            // Move gun position
             transform.position = transform.parent.position + leftPosition;
+            particleSys.transform.position = transform.parent.position + leftPosition;            
+            particleSys.transform.rotation = Quaternion.Euler(180f, 90f, 0f);
+            
             sprite.flipX = true;
         }
         else
         {
+            // Move gun position
             transform.position = transform.parent.position + rightPosition;
+            particleSys.transform.position = transform.parent.position + rightPosition;
+            particleSys.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
             sprite.flipX = false;
+        }
+
+        // Check if Freezegun is active
+        if(Input.GetKey(KeyCode.F))
+        {
+            p.Play();
+        }
+        else
+        {
+            p.Stop();
         }
     }
 }
